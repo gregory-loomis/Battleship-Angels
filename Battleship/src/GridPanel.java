96533037a2210a3e93f619a7c1373449c2589563
shreemayichityala-ops <shreemayi.chityala@gmail.com>
2026-01-30 14:14:10 -0500

    titlePanel attempt
    Contains 4 errors

--- Battleship/src/GridPanel.java
+++ Battleship/src/GridPanel.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
+
 
 public class GridPanel extends JPanel {
 
@@ -9,7 +12,7 @@ public class GridPanel extends JPanel {
         buttons = new JButton[10][10];
 
         // Simple grid layout only
-        setLayout(new GridLayout(10, 10));
+        setLayout(new BorderLayout(10, 10));
 
         // Add buttons to grid
         for (int row = 0; row < 10; row++) {
@@ -17,13 +20,40 @@ public class GridPanel extends JPanel {
                 JButton btn = new JButton();
                 btn.setPreferredSize(new Dimension(40, 40));
                 buttons[row][col] = btn;
-                add(btn);
+                gridPanel.add(btn);
             }
         }
+
+        add(gridPanel, BorderLayout.WEST);
+        JPanel rightPanel = new JPanel();
+        rightPanel.setPreferredSize(new Dimension(250,400));
+
+        add(rightPanel, BorderLayout.CENTER);
+
+
     }
 
     // Getter for later use (controller can add listeners)
     public JButton[][] getButtons() {
         return buttons;
     }
+
+    public JPanel createTitlePanel() {
+        JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        JLabel gameTitle = new JLabel("Battle Ship", JLabel.CENTER);
+        gameTitle.setFont(new Font("Arial", Font.BOLD, 36));
+        
+        titlePanel.setBorder(new EmptyBorder(20,0,10,0));
+
+        return titlePanel;
+    }
+
+    public JButton[][] getButtons(){
+        return buttons;
+    }
 }
+//Task
+    //Add heading "Battle Ship"
+    //Shift grid to the left
+    //add panel to the left
