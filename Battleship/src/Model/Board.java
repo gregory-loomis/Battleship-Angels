9c4c21daca2f7052aa02a8961f8662bb6e963c01
Chaudhry.Aayan <aayanch1984@gmail.com>
2026-01-30 14:01:02 -0500

    added random ship placement

--- Battleship/src/Model/Board.java
+++ Battleship/src/Model/Board.java
@@ -1,7 +1,8 @@
 package Model;
 
-
-import java.util.HashMap;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Board{
@@ -13,10 +14,7 @@ public class Board{
     //>0 = unhit ship number present
     private int[][] board;
 
-    //corresponds given coordinate to the reference to the ship object
-    private HashMap<int[],Ship> correspondence = new HashMap<>();
-
-    private Ship[] ships;
+    private ArrayList<Ship> ships = new ArrayList<>();
 
     public Board(int w,int h){
         width = w;
@@ -32,7 +30,7 @@ public class Board{
     public int getWidth() {return width;}
     public int getHeight() {return height;}
     public int[][] getBoard() {return board;}
-    public Ship[] getShips() {return ships;}
+    public ArrayList<Ship> getShips() {return ships;}
 
     //sets board to 0s
     public void clearBoard(){
@@ -88,6 +86,86 @@ public class Board{
                 return false;
         }
 
+    }
+
+    //places ship on board
+    //assumes given spot is valid
+    public void placeShip(int vertical_index, int horizontal_index, int len, String orientation,int id) throws Exception{
+        orientation = orientation.toLowerCase();
+
+        //errors if the orientation isnt h for horizontal or v for vertical
+        if (!orientation.equals("h") && !orientation.equals("v")) {
+            throw new Exception("Orientation must be (H)orizontal or (V)ertical");
+        }
+
+        switch (orientation) {
+            //vertical ship
+            case "v":
+
+                for (int v = vertical_index;v<vertical_index+len;v++){
+                    //goes through each square in the length of the ship vertically and fills it
+                    board[v][horizontal_index] = id;
+
+                }
+                break;
+
+
+            //horizontal ship
+            case "h":
+
+                for (int h = horizontal_index;h<horizontal_index+len;h++){
+                    //goes through each square in the length of the ship horizontally and fills it
+                    board[vertical_index][h] = id;
+                }
+                break;
+
+            default:
+        }
+
+        /*
+        TODO: Make ship and add to list of ships
+        Ship s = new Ship(asofjasifjasfio);
+        ships.add(s);
+        */
+    }
+
+
+
+    public void randomPlacement(int[] sizes) throws Exception{
+        clearBoard();
+        int[] copy = Arrays.copyOf(sizes, sizes.length);
+        Arrays.sort(copy);
+
+        for (int i = copy.length-1;i>=0;i--){
+            int len = copy[i];
+            int vertical_index;
+            int horizontal_index;
+            String orientation;
+            do {
+                vertical_index = (int) (Math.random()*board.length);
+                horizontal_index = (int) (Math.random()*board[0].length);
+                if (Math.random()<.5) orientation = "h";
+                else orientation = "v";
+            } while (!validPlacement(vertical_index, horizontal_index, len, orientation));
+
+            placeShip(vertical_index, horizontal_index, len, orientation, i+1);
+
+        }
+    }
+
+    @Override
+    public String toString(){
+        String ret = "";
+
+        for (int[] row : board){
+            for (int item: row){
+                ret+=item;
+                ret+=" ";
+            }
+            ret+="\n";
+        }
+
+        return ret;
     }
 
 }
